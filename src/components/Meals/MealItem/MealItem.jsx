b3607fbdf20d6b4d4acbe145ec0a188860fab109
HMP1993 <mr.momen.mrm@gmail.com>
2022-12-30 18:01:06 +0000

    MealItem rendered in Available meals

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import classes from "./MealItem.module.css";
 
 const MealItem = (props) => {
 const price = `$${props.price.toFixed(2)}`
 
   return (
-    <li>
+    <li className={classes.meal}>
       <div>
         <h1>{props.name} </h1>
-        <div>{props.description} </div>
-        <div>{price} </div>
+        <div className={classes.description}>{props.description} </div>
+        <div className={classes.price}>{price} </div>
       </div>
       <div></div>
     </li>
