f3a4e5d2b8d488e3b3b310419f78d27b0b547436
HMP1993 <mr.momen.mrm@gmail.com>
2023-01-05 21:37:33 +0000

    cart amount number is correct now

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,7 +1,6 @@
-import React from "react";
 import Card from "../UI/Card";
-import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
   {
@@ -29,15 +28,18 @@ const DUMMY_MEALS = [
     price: 18.99,
   },
 ];
+
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
     />
   ));
+
   return (
     <section className={classes.meals}>
       <Card>
