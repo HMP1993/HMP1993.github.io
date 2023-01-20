77bdaa0265e07aac80a1a8fc671545013e1df4b0
HMP1993 <mr.momen.mrm@gmail.com>
2023-01-20 10:41:53 +0000

    final changes done. ready to go

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,20 +1,21 @@
-import React, { Fragment } from "react";
-import Banner from "../../assets/top-view-green-leaf-dolma-with-seasonings-dark-surface.jpg";
-import classes from "./Header.module.css";
+import { Fragment } from "react";
+
 import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from "../../assets/top-view-green-leaf-dolma-with-seasonings-dark-surface.jpg";
+import classes from "./Header.module.css";
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>Bison</h1>
+        <h1>ReactMeals</h1>
         <HeaderCartButton onClick={props.onShowCart} />
-        
       </header>
       <div className={classes["main-image"]}>
-        <img src={Banner} alt="cover img" />
+        <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </Fragment>
   );
 };
+
 export default Header;
