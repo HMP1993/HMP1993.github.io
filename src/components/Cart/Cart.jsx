49b4fc39d197d7676f7331bf7c0da0b2e3eb81b7
HMP1993 <mr.momen.mrm@gmail.com>
2023-01-20 10:11:23 +0000

    totalAmount fixed

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,7 +4,8 @@ import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 const Cart = (props) => {
-  const cartCtx = useContext(CartContext)
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
@@ -14,14 +15,16 @@ const Cart = (props) => {
   );
 
   return (
-    <Modal onHideCart={props.onHideCart} >
+    <Modal onHideCart={props.onHideCart}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.88</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onHideCart}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
